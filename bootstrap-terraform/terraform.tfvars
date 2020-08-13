fb4085e4fac1cf5cdd4363b2f338777ae45998ca
kylegreen <kyle.green@thoughtworks.com>
2020-08-12 20:58:18 -0600

    Initial commit (tf bootstrap dir)

---
+++ bootstrap-terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+# GCP Settings
+gcp_project   = "archtek-wordpress"
+gcp_region    = "us-west1"
+gcp_auth_file = "./archtek-wordpress-bd12e6633ade.json"
+bucket_name   = "archtek-wordpress"
+storage_class = "REGIONAL"
---
+++ bootstrap-terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the Google Storage Bucket to create"
+}
+variable "storage_class" {
+  type        = string
+  description = "The storage class of the Storage Bucket to create"
+}
